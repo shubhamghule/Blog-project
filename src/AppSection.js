848821c288af5dcc42d4a7fbdc94e994cf0e9705
Shubham Ghule <shubhamghule03@gmail.com>
2020-07-23 18:10:36 +0530

    Complete template added

--- src/AppSection.js
+++ src/AppSection.js
@@ -6,7 +6,7 @@ const AppSection = () =>{
         <div className="bg-success text-center">
           <p className="display-4 text-white p-4">Discover the amazing new app</p>
           <p className="text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, rem?
+            How's Your Mood today ?
           </p>
           <div className="row  p-4">
             <div className="col-6 text-right">
--- src/FooterSection.js
+++ src/FooterSection.js
@@ -3,10 +3,10 @@ import React from "react";
 const Footer =()=>{
     return (
             //  <!-- footer section --> 
-            <section className="footer  bg-dark text-white">
+            <section className="footer text-center bg-dark text-white">
             <p className="lead container">Contact us at Instagram</p>
-            <p className="lead container">You can react via email too</p>
-            <p className="lead container">NO COPYRIGHT</p>
+            <p className="lead container">You can reach at ghuleshubham2013@gmail.com</p>
+            <p className="lead container">Created by Shubham Ghule</p>
             </section>
             // <!-- footer section ends -->
     )
